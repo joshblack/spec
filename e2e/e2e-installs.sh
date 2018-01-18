bfad5aa479bede4f07cff1845b4d788a9d98fb50
Josh Black <joshblack@us.ibm.com>
2018-01-18 16:02:22 -0600

    chore: add clean-up to e2e install step

--- e2e/e2e-installs.sh
+++ e2e/e2e-installs.sh
@@ -3,16 +3,31 @@
 # Start in e2e/ even if run from root directory
 cd "$(dirname "$0")"
 
+# CLI and app temporary locations
+# http://unix.stackexchange.com/a/84980
+temp_app_path=`mktemp -d 2>/dev/null || mktemp -d -t 'temp_app_path'`
 custom_registry_url=http://localhost:4873
+original_npm_registry_url=`npm get registry`
+original_yarn_registry_url=`yarn config get registry`
+
+function cleanup {
+  echo 'Cleaning up.'
+  cd "$root_path"
+  rm -rf "$temp_app_path"
+  npm set registry "$original_npm_registry_url"
+  yarn config set registry "$original_yarn_registry_url"
+}
 
 # Error messages are redirected to stderr
 function handle_error {
   echo "$(basename $0): ERROR! An error was encountered executing line $1." 1>&2;
+  cleanup
   echo 'Exiting with error.' 1>&2;
   exit 1
 }
 
 function handle_exit {
+  cleanup
   echo 'Exiting without error.' 1>&2;
   exit
 }
@@ -37,6 +52,15 @@ set -x
 cd ..
 root_path=$PWD
 
+if hash npm 2>/dev/null
+then
+  npm i -g npm@latest
+  npm cache clean || npm cache verify
+fi
+
+# Bootstrap monorepo
+yarn
+
 # ******************************************************************************
 # First, publish the monorepo.
 # ******************************************************************************
@@ -56,3 +80,6 @@ npx npm-cli-login@0.0.10 -u user -p password -e user@example.com -r "$custom_reg
 
 # Publish the monorepo
 ./tasks/publish.sh --yes --force-publish=* --skip-git --cd-version=prerelease --exact --npm-tag=latest
+
+# Cleanup
+cleanup
