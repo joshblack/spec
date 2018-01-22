589823e389b641f82100311da83fa309fe9aa6ca
Josh Black <joshblack@us.ibm.com>
2018-01-21 18:11:02 -0600

    feat(cli): update display success step

--- packages/spec-cli/scripts/init/middleware/display-success.js
+++ packages/spec-cli/scripts/init/middleware/display-success.js
@@ -3,21 +3,24 @@
 /* eslint-disable no-console */
 
 const chalk = require('chalk');
+const path = require('path');
 
 module.exports = program => {
-  const useYarn = !program.useNpm;
-  const displayedCommand = useYarn ? 'yarn' : 'npm';
+  const { useNpm } = program;
+  const displayedCommand = useNpm ? 'npm' : 'yarn';
 
   console.log();
-  console.log('Success!');
+  console.log(chalk.green('Success!'));
+  console.log();
+  console.log(`${chalk.cyan('@spec/cli')} just created your project at:`);
+  console.log(`  ${program.cwd}`);
+  console.log();
   console.log('Inside that directory, you can run several commands:');
   console.log();
-  console.log(chalk.cyan(`  ${displayedCommand}${useYarn ? '' : ' run'} dev`));
+  console.log(chalk.cyan(`  ${displayedCommand}${useNpm ? ' run' : ''} dev`));
   console.log('    Starts the development server.');
   console.log();
-  console.log(
-    chalk.cyan(`  ${displayedCommand} ${useYarn ? '' : 'run '}build`)
-  );
+  console.log(chalk.cyan(`  ${displayedCommand} ${useNpm ? 'run ' : ''}build`));
   console.log('    Bundles the app into static files for production.');
   console.log();
   console.log(chalk.cyan(`  ${displayedCommand} test`));
@@ -25,6 +28,9 @@ module.exports = program => {
   console.log();
   console.log('We suggest that you begin by typing:');
   console.log();
+  console.log(
+    `  ${chalk.cyan(`cd ${path.relative(process.cwd(), program.cwd)}`)}`
+  );
   console.log(`  ${chalk.cyan(`${displayedCommand} dev`)}`);
   console.log();
   console.log('Happy hacking!');
